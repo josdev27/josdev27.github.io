d1a4ea0a6b59717acc1cebfdf18cf14b700b9eb8
josdev27 <47904226+josdev27@users.noreply.github.com>
2021-08-26 19:22:45 +0200

    First version

---
+++ build/precache-manifest.d8a9ee554c026442c73c89e551d79833.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11172b4b1584da0ab6f7bcc063b3b667",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "297d4644d3e90ec63860",
+    "url": "/portfolio/static/css/3.69a0f723.chunk.css"
+  },
+  {
+    "revision": "a4e0c8e1d7e674d22b48",
+    "url": "/portfolio/static/css/4.f7b0c6ac.chunk.css"
+  },
+  {
+    "revision": "01b47677bcea5c8a42a7",
+    "url": "/portfolio/static/css/main.20df68df.chunk.css"
+  },
+  {
+    "revision": "f73e80352ef842f3b1e0",
+    "url": "/portfolio/static/js/2.37365907.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.37365907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297d4644d3e90ec63860",
+    "url": "/portfolio/static/js/3.c6573fff.chunk.js"
+  },
+  {
+    "revision": "a4e0c8e1d7e674d22b48",
+    "url": "/portfolio/static/js/4.9b3a31a8.chunk.js"
+  },
+  {
+    "revision": "01b47677bcea5c8a42a7",
+    "url": "/portfolio/static/js/main.e70a04a6.chunk.js"
+  },
+  {
+    "revision": "753249d897e7b496d495",
+    "url": "/portfolio/static/js/runtime-main.5428d35f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "bca5127f79ec90a87f9028544b9c12ff",
+    "url": "/portfolio/static/media/accentureLogo.bca5127f.jpg"
+  },
+  {
+    "revision": "c17b144af3ef1ce3dcc536ecb567eafb",
+    "url": "/portfolio/static/media/carlosLogo.c17b144a.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "32cb6744af72b9b7ac840ab71a228d37",
+    "url": "/portfolio/static/media/upmLogo.32cb6744.png"
+  }
+]);
